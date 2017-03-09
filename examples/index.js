3b39c7dd0ecea1bf3b20027ae2b716e2b295a4eb
Raymond Ho <rayho@paypal.com>
2017-03-09 22:52:56 +0800

    working on the not selector but this works

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,31 @@
-import carousell from '../src/carousell';
+var casperJS = require('casper');
 
-console.log(carousell.greet());
+function run() {
+
+    var casper = casperJS.create();
+    var url = casper.cli.options.url;
+    var username = casper.cli.options.username;
+    var password = casper.cli.options.password;
+    casper.start();
+
+    casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36');
+
+    casper.thenOpen('https://carousell.com/login/?next=' + url, function () {
+        this.fillSelectors('form.login-form', {
+            'input[name="username"]': username,
+            'input[name="pass"]': password,
+        }, true);
+    });
+    casper.wait(3000, function () {
+        this.echo(this.getCurrentUrl());
+        this.echo(this.getHTML('#likeButton'));
+        // this.click('#likeButton:not(.pdt-buy-box-like-button-success)');
+        this.click('#likeButton');
+        casper.wait(2000, function () {
+            this.echo(this.getHTML('#likeButton'));
+        });
+    });
+
+    casper.run();
+} 
+run();
