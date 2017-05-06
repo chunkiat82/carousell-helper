f8ac74323cdb42098ee5d81c1bc0aff1bbc77dba
Raymond Ho <chunkiat82@gmail.com>
2017-05-06 21:00:16 +0800

    moving to protractor

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-### [0.0.1](https://github.com/chunkiat82/carousell/releases/tag/v0.0.1)
-
-- The first release
---
+++ carousell-spec.js
@@ -0,0 +1,43 @@
+var accounts = require('./accounts.json');
+var carousell = require('./carousell.json');
+
+describe('angularjs homepage todo list', function() {
+
+  Object.keys(accounts).forEach(function(account){
+    it('should add a todo', function() {
+
+        var usernameString = account;
+        var passwordString = accounts[account];
+        browser.waitForAngularEnabled(false);
+
+        browser.get('https://carousell.com/login/?next=');
+
+        var usernameField = element(by.id('username'));
+        var passwordField = element(by.id('password'));
+        var submitButton = element(by.xpath('//*[@id="root"]/div/div[1]/div/form/button'));
+
+        usernameField.sendKeys(usernameString);
+        passwordField.sendKeys(passwordString);
+        submitButton.click();
+        browser.sleep(500);
+        browser.get(carousell.url);
+
+        var genericLikeButton = element(
+          by.js(function(){
+            return document.querySelector('#likeButton') || null;
+          })
+        );
+
+        browser.isElementPresent(genericLikeButton).then(function(){
+          var likeButton = element(
+            by.js(function(){
+              return document.querySelector('#likeButton:not(.pdt-buy-box-like-button-success)') || null;
+            })
+          );
+          likeButton.click();
+
+        });
+        browser.sleep(200);
+    });
+  });
+});
