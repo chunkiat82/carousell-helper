53d67198b0fb87ac727e92a331b51b2f28ba9381
Raymond Ho <chunkiat82@gmail.com>
2017-06-05 17:58:34 +0800

    adding timeout to 20s

--- carousell-spec.js
+++ carousell-spec.js
@@ -1,43 +1,47 @@
 var accounts = require('./accounts.json');
 var carousell = require('./carousell.json');
 
-describe('Carousell Helper to Like Page', function() {
+describe('Carousell Helper to Like Page', function () {
 
-  Object.keys(accounts).forEach(function(account){
-    it('should add a todo', function() {
+  beforeEach(function () {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+  });
 
-        var usernameString = account;
-        var passwordString = accounts[account];
-        browser.waitForAngularEnabled(false);
+  Object.keys(accounts).forEach(function (account) {
+    it('should add a todo', function () {
 
-        browser.get('https://carousell.com/login/?next=');
+      var usernameString = account;
+      var passwordString = accounts[account];
+      browser.waitForAngularEnabled(false);
 
-        var usernameField = element(by.id('username'));
-        var passwordField = element(by.id('password'));
-        var submitButton = element(by.xpath('//*[@id="root"]/div/div[1]/div/form/button'));
+      browser.get('https://carousell.com/login/?next=');
 
-        usernameField.sendKeys(usernameString);
-        passwordField.sendKeys(passwordString);
-        submitButton.click();
-        browser.sleep(500);
-        browser.get(carousell.url);
+      var usernameField = element(by.id('username'));
+      var passwordField = element(by.id('password'));
+      var submitButton = element(by.xpath('//*[@id="root"]/div/div[1]/div/form/button'));
 
-        var genericLikeButton = element(
-          by.js(function(){
-            return document.querySelector('#likeButton') || null;
+      usernameField.sendKeys(usernameString);
+      passwordField.sendKeys(passwordString);
+      submitButton.click();
+      browser.sleep(500);
+      browser.get(carousell.url);
+
+      var genericLikeButton = element(
+        by.js(function () {
+          return document.querySelector('#likeButton') || null;
+        })
+      );
+
+      browser.isElementPresent(genericLikeButton).then(function () {
+        var likeButton = element(
+          by.js(function () {
+            return document.querySelector('#likeButton:not(.pdt-buy-box-like-button-success)') || null;
           })
         );
+        likeButton.click();
 
-        browser.isElementPresent(genericLikeButton).then(function(){
-          var likeButton = element(
-            by.js(function(){
-              return document.querySelector('#likeButton:not(.pdt-buy-box-like-button-success)') || null;
-            })
-          );
-          likeButton.click();
-
-        });
-        browser.sleep(200);
+      });
+      browser.sleep(200);
     });
   });
 });
